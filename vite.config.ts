711a571b0c075db9ee0cac4cd723c1a66db3bccb
OtorresO <asantic59@gmail.com>
2024-03-04 18:48:11 -0500

    Fix:Change base public path

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/Translate-App/'
+
+  /* WHITOUT DOMAIN */
+  /* base:'/Translate-App/' */
+
+  /* WITH DOMAIN */
+  base: '/'
+
 })
